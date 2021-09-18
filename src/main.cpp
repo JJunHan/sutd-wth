d213cb87c41f35cc61d096b7b60df4b7b96e0a5c
Jun Han <junhanj@hotmail.com>
2021-09-18 15:29:41 +0800

    added ultrasound and ir sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,55 @@
 #include <Arduino.h>
 
-const int ledPin = 23;
-const int irPin = 4;
+#define LEDPIN 23
+#define IRPIN 4
+#define TRIGPIN 5
+#define ECHOPIN 18
+#define SOUND_SPEED 0.034 // in micro secs
+
 int IRsensorvalue;
+long duration;
+float distanceCm;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   pinMode(ledPin, OUTPUT);
-  pinMode(irPin, OUTPUT);
+  pinMode(IRPIN, OUTPUT);
+  pinMode(TRIGPIN, OUTPUT); // Sets the trigPin as an Output
+  pinMode(ECHOPIN, INPUT); // Sets the echoPin as an Input
 }
 
 void loop() {
-  IRsensorvalue = analogRead(irPin);
+  IRsensorvalue = analogRead(IRPIN);
   if(IRsensorvalue < 200) {
-    digitalWrite(ledPin, HIGH);
-    delay(1000); 
+    digitalWrite(LEDPIN, HIGH);
+    //delay(1000); 
   }
   else{
-    digitalWrite(ledPin, LOW);
+    digitalWrite(LEDPIN, LOW);
   }
+
+  // Clears the trigPin
+  digitalWrite(TRIGPIN, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(TRIGPIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIGPIN, LOW);
   
-  // float voltage = sensorvalue * (3.3 / 4096.0);
-  // Serial.println(sensorvalue);
-  // Serial.println(voltage);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(ECHOPIN, HIGH);
+  
+  // Calculate the distance
+  distanceCm = duration * SOUND_SPEED/2;
+  
+  // Prints the distance in the Serial Monitor
+  Serial.print("Distance (cm): ");
+  Serial.println(distanceCm);
+  
+  //float voltage = IRsensorvalue * (3.3 / 4096.0);
+  //Serial.println(IRsensorvalue);
+  //Serial.println(voltage);
+  //Serial.println(duration);
   delay(500);
 }
